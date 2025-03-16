91159c1a400ba7270b646a3d8bcd4ee184bc5403
MohitNamdev22 <namdevmohit0@gmail.com>
2025-03-16 17:18:18 +0530

    Operator check program

---
+++ Java_Assignment/DatatypesOperators/OperatorsCheck.java
@@ -0,0 +1,29 @@
+package Java_Assignment.DatatypesOperators;
+
+public class OperatorsCheck {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        boolean x = true, y = false;
+
+        System.out.println("Arithmetic Operators:");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+        System.out.println("\nRelational Operators:");
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+
+        System.out.println("\nLogical Operators:");
+        System.out.println("x && y = " + (x && y));
+        System.out.println("x || y = " + (x || y));
+        System.out.println("!x = " + !x);
+
+
+    }
+    
+}
